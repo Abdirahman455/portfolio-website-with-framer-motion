84353df5bb56e0b4c0cee4b0e579a08a6970fece
Abdirahman Muse <musea186@gmail.com>
2025-12-23 14:09:54 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+    emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
+  }, []);
+
+  return (
+    <div className={`app ${isLoaded ? "loaded" : ""}`}>
+      <Navbar />
+
+      <Hero />
+      <Projects />
+      <Contact />
+
+      <>
+        <p>&copy; 2025 Abdirahman Muse. All rights reserved</p>
+      </>
+    </div>
+  );
+}
+
+export default App;
